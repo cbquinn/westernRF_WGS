db0dbf8779f6a3bb9b2cfea7c53a7d325c35bdd9
Cate Quinn <45103654+cbquinn@users.noreply.github.com>
2024-02-28 16:43:54 -0700

    Update and rename 0.3_clean.md to 0.3_clean.sh

--- 0.3_clean.sh
+++ 0.3_clean.sh
@@ -1,55 +1,6 @@
-
-make list of unique samples
-```R
-library(tidyverse)
-f <- read_tsv("/group/ctbrowngrp3/scratch/cbquinn/fox/fastqlist.txt", col_names=c("JOB",	"RUN",	"RFID",	"SID",	"BASENAME",	"LIB",	"PLATFORM", "POP")) 
-
-mylist <- f %>%
-	select(RFID, SID, POP) %>%
-	distinct() %>%
-	unite("sampleID", c(POP, SID), sep="_", remove=FALSE) %>%
-	mutate(keep=case_when(POP %in% c("SVNN", "SNF1", "LAS2019") ~ FALSE,
-	SID == "F12-225" ~ FALSE,
-	TRUE ~ TRUE)) %>% 
-	arrange(RFID)
-
-# RF samples 1-33 should be arranged by sampleID
-fround1 <- f[1:54,]
-
-mylist <- bind_rows( mylist %>%
-	filter(SID %in% unique(fround1$SID)) %>%
-	arrange(sampleID) ,
-	mylist %>%
-		filter(!SID %in% unique(fround1$SID)) %>%
-		arrange(sampleID) 
-		)
-
-write_tsv(mylist,"/group/ctbrowngrp3/scratch/cbquinn/fox/bams/samplelist.txt", col_names=FALSE)
-```
-
-List looks like this (but without headers):
-```
-RFID    sampleID        SID     POP     keep
-RF1     SN_S11-0008     S11-0008        SN      TRUE
-RF2     SNF1_S15-0575   S15-0575        SNF1    FALSE
-RF3     RM_S13-3301     S13-3301        RM      TRUE
-RF4     RM_S13-3312     S13-3312        RM      TRUE
-RF5     LAS_M01 M01     LAS     TRUE
-RF6     LAS_F01 F01     LAS     TRUE
-...
-```
-
-ones that are ready: 1-18,19-20,23-33
-ran 1, 5, 13, 23, 26,31
-then ran 2-4,6-12,14-20,24-25,27-30,32,33
-for run5, need to run 34-43
-had to rerun these because fo the screwy bam swap snafu:  job arrays 13,17, 30 
-
-/group/ctbrowngrp2/cbquinn/fox4/slurmscripts/bwa_clean.sh
-```sh
 #!/bin/bash -l
 #SBATCH --job-name=clean
-#SBATCH --array=13,17,30
+#SBATCH --array=1-34
 #SBATCH --time 23:40:00
 #SBATCH --mem=16GB
 #SBATCH -p bmm
@@ -64,6 +15,10 @@ set -o nounset
 set -o errexit
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+
+# LIST IS A TAB-DELIMITED LIST FORMATED WITH THE FOLLOWING HEADERS
+# RFID    sampleID        SID     POP
+
 
 LIST=/group/ctbrowngrp3/scratch/cbquinn/fox/bams/samplelist.txt
 SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $LIST | cut -f2)
@@ -163,149 +118,7 @@ TIMESPEND=$(($ENDTIME - $STARTTIME))
 ((sec=TIMESPEND%60,TIMESPEND/=60, min=TIMESPEND%60, hrs=TIMESPEND/60))
 timestamp=$(printf "%d:%02d:%02d" $hrs $min $sec)
 echo "All done. Took $timestamp hours:minutes:seconds to complete..."
-```
 
-/group/ctbrowngrp2/cbquinn/fox4/slurmscripts/bwa_clean_postfarmupdate.sh
-```sh
-#!/bin/bash -l
-#SBATCH --job-name=clean
-#SBATCH --array=13,17,30
-#SBATCH --time 2-00:00:00
-#SBATCH --mem=16GB
-#SBATCH -p bmm
-#SBATCH -A ctbrowngrp
-#SBATCH -o /group/ctbrowngrp2/cbquinn/fox4/slurmlogs/clean_%a.out
-#SBATCH -e /group/ctbrowngrp2/cbquinn/fox4/slurmlogs/clean_%a.err
-
-STARTTIME=$(date +%s)
-
-# make things fail on errors
-set -o nounset
-set -o errexit
-
-echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
-
-LIST=/group/ctbrowngrp3/scratch/cbquinn/fox/bams/samplelist.txt
-SAMPLE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $LIST | cut -f2)
-TEMPDIR=/group/ctbrowngrp3/scratch/cbquinn/fox/bams/
-OUTDIR=/group/ctbrowngrp2/cbquinn/fox4/0_bams/
-REF=/group/ctbrowngrp2/cbquinn/fox4/ref/GCF_018345385.1/GCF_018345385.1_ASM1834538v1_genomic.fna
-BED=/group/ctbrowngrp2/cbquinn/fox4/ref/GCF_018345385.1/autosomes.bed
-
-#module load samtools/1.16.1 # for most samples before farm reboot
-module load samtools/1.14 # for 13,17,30 after farm reboot
-module load picard
-module unload jdk/17.0.1
-module load deprecated/jdk/1.8.0.121
-
-cd $TEMPDIR
-
-# merge split bams
-
-INPUTS_LIST=$( cat <(ls ${TEMPDIR}chunk/readgroups/${SAMPLE}*.bam))
-INPUTS=$(echo $INPUTS_LIST | tr "\n" " ")
-INPUTS_NUMB=$(echo $INPUTS_LIST | wc -l)
-
-echo "Samtools will now merge $INPUTS_NUMB files:"
-echo "samtools merge -f ${TEMPDIR}merged/${SAMPLE}.bam $INPUTS"
-
-samtools merge -f ${TEMPDIR}merged/${SAMPLE}.bam $INPUTS
-
-# mark duplicates
-
-echo "marking duplicates..."
-mkdir -p deduped
-picard MarkDuplicates \
-    VALIDATION_STRINGENCY=LENIENT \
-      I=${TEMPDIR}merged/${SAMPLE}.bam \
-      O=${TEMPDIR}deduped/${SAMPLE}.bam \
-      M=${TEMPDIR}deduped/qcmetrics/${SAMPLE}_duplicationMETRICS.txt
-
-# add read groups again
-RFID=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $LIST | cut -f1)
-SID=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $LIST | cut -f3)
-POP=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $LIST | cut -f4)
-
-echo "adding readgroups... again"
-picard AddOrReplaceReadGroups \
-    I=${TEMPDIR}deduped/${SAMPLE}.bam  \
-    O=${TEMPDIR}readgroups/${SAMPLE}.bam \
-    RGID=${RFID}_${SID} RGLB=merged RGPL=illumina RGPU=NULL RGSM=${RFID}_${SID} \
-    VALIDATION_STRINGENCY=LENIENT
-
-# indel realignment
-module unload deprecated/jdk/1.8.0.121
-module unload picard
-module load gatk/3.8.1
-module load deprecated/jdk/1.8.0.121
-
-echo "indexing bam"
-samtools index ${TEMPDIR}readgroups/${SAMPLE}.bam
-
-echo "realigning indels..."
-
-java -Xmx4g -jar ${GATK} \
--T RealignerTargetCreator \
--nt 4 \
--R ${REF} \
--I ${TEMPDIR}readgroups/${SAMPLE}.bam \
--o ${TEMPDIR}deduped/intervals/${SAMPLE}.intervals \
--log ${TEMPDIR}deduped/intervals/${SAMPLE}.intervals.log
-
-java -Xmx4g -jar ${GATK} \
--T IndelRealigner \
--R ${REF} \
--I ${TEMPDIR}readgroups/${SAMPLE}.bam \
--targetIntervals ${TEMPDIR}deduped/intervals/${SAMPLE}.intervals \
--o ${OUTDIR}indelrealigned/${SAMPLE}.bam
-
-echo "indexing..."
-samtools index ${OUTDIR}indelrealigned/${SAMPLE}.bam
-
-module unload gatk/3.8.1
-module load picard
-
-echo "getting stats from GATK..."
-# collect allignment metrics
-picard CollectAlignmentSummaryMetrics \
-          R=${REF} \
-          I=${OUTDIR}indelrealigned/${SAMPLE}.bam \
-          O=${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_summaryMETRICS.txt \
-          VALIDATION_STRINGENCY=LENIENT
-
-echo "getting stats from samtools..."
-
-# count total reads
-samtools view -L $BED -c ${OUTDIR}indelrealigned/${SAMPLE}.bam > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_NoReads_tot.flagstat
-
-# count number of good reads: exclude umapped reads, secondary and chimeric reads, singleton reads, and clones
-samtools view -F 1796 -L $BED -c ${OUTDIR}indelrealigned/${SAMPLE}.bam > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_NoReads_1796.flagstat
-
-# count number of good reads with mapq > 30: exclude umapped reads, secondary and chimeric reads, singleton reads, and clones
-samtools view -F 1796 -q 30 -L $BED -c ${OUTDIR}indelrealigned/${SAMPLE}.bam > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_NoReads_1796q30.flagstat
-
-# count number of reads that map to more than one location and/or have an umapped mate
-samtools view -f 2312 -L $BED -c ${OUTDIR}indelrealigned/${SAMPLE}.bam > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_NoReads_2312.flagstat
-
-# count number of unmapped reads
-samtools view -f 4 -L $BED -c ${OUTDIR}indelrealigned/${SAMPLE}.bam > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_NoReads_4.flagstat
-
-# echo "getting  histogram of mapping quality..."
-# for "good" reads (co1 is mapQ, col2 is no. reads)
-samtools view -F 1796 -L $BED ${OUTDIR}indelrealigned/${SAMPLE}.bam | cut -f 5 | sort | uniq  -c | sort -n | awk '{printf("%s\t%d\n",$2,$1);}' > ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_mapq.dist
-
-echo "getting mean depth of coverage..."
-# (default is to exclude reads with flag 1796)
-conda activate mosdepth
-mosdepth -n --fast-mode --by 50000 ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE} ${OUTDIR}indelrealigned/${SAMPLE}.bam
-mosdepth -n --fast-mode --by 50000 --mapq 30 ${OUTDIR}indelrealigned/qcmetrics/${SAMPLE}_mapq30 ${OUTDIR}indelrealigned/${SAMPLE}.bam
-
-ENDTIME=$(date +%s)
-TIMESPEND=$(($ENDTIME - $STARTTIME))
-((sec=TIMESPEND%60,TIMESPEND/=60, min=TIMESPEND%60, hrs=TIMESPEND/60))
-timestamp=$(printf "%d:%02d:%02d" $hrs $min $sec)
-echo "All done. Took $timestamp hours:minutes:seconds to complete..."
-```
 
 
 ### get depth and read stats
