f4139c148750f682b44eb42dc485d65bf46aed27
Cate Quinn <45103654+cbquinn@users.noreply.github.com>
2024-02-28 17:06:21 -0700

    Update and rename 3.1_bcftools_ROH.md to 3.1_bcftools_ROH.sh

--- 3.1_bcftools_ROH.sh
+++ 3.1_bcftools_ROH.sh
@@ -1,7 +1,3 @@
-Run with default allele frequency and mean recombinatino 
-
-/group/ctbrowngrp2/cbquinn/fox4/slurmscripts/bcftools_ROH.sh
-```
 #!/bin/bash -l
 #SBATCH --job-name=bcfroh
 #SBATCH --time 1-00:00:00
@@ -11,11 +7,14 @@ Run with default allele frequency and mean recombinatino
 #SBATCH -o /group/ctbrowngrp2/cbquinn/fox4/slurmlogs/bcftools_ROH.out
 #SBATCH -e /group/ctbrowngrp2/cbquinn/fox4/slurmlogs/bcftools_ROH.err
 
+# run bcftools/roh with default allele frequency and mean recombinatino 
 
 cd /group/ctbrowngrp2/cbquinn/fox4/1_vcfs/variant/n34
 OUTDIR=/group/ctbrowngrp2/cbquinn/fox4/3_ROH/bcftools
 
 module load bcftools/1.14
+
+# this creates output files for filters A,B,C,D (used A in final analysis)
 
 for i in $(ls *_masked.vcf.gz)
 do
@@ -26,7 +25,3 @@ end=`date +%s`
 runtime=$((end-start))
 echo "RUNTIME for $i: $runtime"
 done
-
-```
-
-
