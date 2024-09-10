da6f31828c0d458e0918b429ea52e162ed0f3707
Cate Quinn <45103654+cbquinn@users.noreply.github.com>
2024-09-10 05:47:35 -0600

    add pops with n = 1

--- 4.9_MSMC_boot_run.sh
+++ 4.9_MSMC_boot_run.sh
@@ -30,9 +30,16 @@ dir="t20_10x1_22x2_1x4_1x6"
 # 64 time segments with 34 free parameters
 mkdir -p $OUTDIR/$dir/boot
 
+# n = 2
 msmc2 -t 16 -i 20 -p $p -I 0,1,2,3 -o ${OUTDIR}/$dir/boot/LAS_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
-
 msmc2 -t 16 -i 20 -p $p -I 4,5,6,7 -o ${OUTDIR}/$dir/boot/ORC_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
 msmc2 -t 16 -i 20 -p $p -I 8,9,10,11 -o ${OUTDIR}/$dir/boot/RM_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
 msmc2 -t 16 -i 20 -p $p -I 12,13,14,15 -o ${OUTDIR}/$dir/boot/WAC_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
 msmc2 -t 16 -i 20 -p $p -I 16,17,18,19 -o ${OUTDIR}/$dir/boot/EAST_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
+# n = 1
+msmc2 -t 16 -i 20 -p $p -I 20,21 -o ${OUTDIR}/$dir/boot/RUS_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
+msmc2 -t 16 -i 20 -p $p -I 22,23 -o ${OUTDIR}/$dir/boot/AK_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
+msmc2 -t 16 -i 20 -p $p -I 24,25 -o ${OUTDIR}/$dir/boot/SN_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
+msmc2 -t 16 -i 20 -p $p -I 16,17 -o ${OUTDIR}/$dir/boot/ECAN_bootstrap_number_${SLURM_ARRAY_TASK_ID}.out ${INDIR}/boot/bootstrap_number_${SLURM_ARRAY_TASK_ID}/*txt
+
+
